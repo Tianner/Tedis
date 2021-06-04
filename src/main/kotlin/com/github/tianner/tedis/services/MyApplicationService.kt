f52cc2c4816aee93d67d3db1e8bd756fb531c5e1
GitHub Action <action@github.com>
2021-06-04 07:01:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/tianner/tedis/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tianner/tedis/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tianner.tedis.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tianner.tedis.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tianner/tedis/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tianner/tedis/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tianner.tedis.services
 
+import com.github.tianner.tedis.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
